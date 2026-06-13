51ef408463c7e1745bb9c8bc79775a77844c4bf4
Ben Cuthbert <214280418+holypinko@users.noreply.github.com>
2026-06-13 16:43:59 +0100

    First Version

---
+++ GuessDoom.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+
+Random random = new Random(); //Initialise Random
+
+int correctNum = random.Next(1, 6); //1-5 random number
+
+int guess;
+
+Console.WriteLine("Pick a number between 1 and 5...\nIf you guess correctly you can play Doom!");
+
+int.TryParse(Console.ReadLine(), out guess); //Parses guess as an integer (So nothing other than an integer is allowed)
+
+while (guess != correctNum)
+{
+    Console.WriteLine("Wrong! Try again!");
+    int.TryParse(Console.ReadLine(), out guess); //Retries the parsing
+}
+
+Console.WriteLine("\nCorrect! Enjoy Doom!\n");
+
+System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo("https://ustymukhman.github.io/WebDOOM/public/") { UseShellExecute = true }); //Sends default browser to WebDOOM
+
+Console.WriteLine("""
+    Credits:
+        Created By: holypinko
+        WebDOOM: https://ustymukhman.github.io/WebDOOM/public/
+    """); //Credits, of course
